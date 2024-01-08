15df9d1753fa390f0a8351dda1833634048e4d10
0xGhazy <hhammdypro@gmail.com>
2024-01-08 21:48:41 +0200

    adding new 2-pointers problems

--- leet-code-solutions/medium/151/Solution.py
+++ leet-code-solutions/medium/151/Solution.py
@@ -4,15 +4,15 @@ class Solution:
         return " ".join([x for x in init_words[::-1] if x != ""])
         
 
-solution = Solution()
-s = "the sky is blue"
-if(solution.reverseWords(s) == "blue is sky the"):
-    print("Pass 1")
+# solution = Solution()
+# s = "the sky is blue"
+# if(solution.reverseWords(s) == "blue is sky the"):
+#     print("Pass 1")
 
-s = "  hello world  "
-if(solution.reverseWords(s) == "world hello"):
-    print("Pass 2")
+# s = "  hello world  "
+# if(solution.reverseWords(s) == "world hello"):
+#     print("Pass 2")
 
-s = "a good   example"
-if(solution.reverseWords(s) == "example good a"):
-    print("Pass 3")
+# s = "a good   example"
+# if(solution.reverseWords(s) == "example good a"):
+#     print("Pass 3")
---
+++ leet-code-solutions/medium/2396/Solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        return False
+
+
